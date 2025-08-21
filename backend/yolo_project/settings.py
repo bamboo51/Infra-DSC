e160cbf747a582234c4cc2076d057d1da9739655
Phongwit Srikokcharoen <pongsatorn2548self@gmail.com>
2025-08-21 18:44:36 +0900

    change env variables

--- backend/yolo_project/settings.py
+++ backend/yolo_project/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise Exception("The SECRET_KEY setting must not be empty.")
 
